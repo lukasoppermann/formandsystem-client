c1115663ece08326ba15cf0e088da6857534fe86
Lukas Oppermann <lukas@vea.re>
2016-09-13 20:06:36 +0200

    add ssl to copra app.blade

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,10 +5,10 @@
         <meta name="description" content="{{$active['page']['description'] or ''}}">
         <meta http-equiv="content-language" content="de">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1,maximum-scale=1">
-        <link rel="shortcut icon" href="{{ asset('/media/favicon.png') }}">
-        <link rel="icon" href="{{ asset('/media/favicon.png') }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('/media/favicon.png', Request::secure()) }}">
+        <link rel="icon" href="{{ asset('/media/favicon.png', Request::secure()) }}" type="image/x-icon">
         <meta name="theme-color" content="rgb(255,210,0)">
-        <link href="{{ asset(env('PATH_PREFIX').elixir('css/app.css')) }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset(env('PATH_PREFIX').elixir('css/app.css'), Request::secure()) }}" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,600" rel="stylesheet" type="text/css">
         @if (env('APP_ENV') !== 'local')
             <script>
@@ -29,6 +29,6 @@
             @yield('content')
         </main>
         @include('layout.footer')
-        <script src='{{ asset(env('PATH_PREFIX').elixir("js/app.js")) }}'></script>
+        <script src='{{ asset(env('PATH_PREFIX').elixir("js/app.js"), Request::secure()) }}'></script>
     </body>
 </html>
