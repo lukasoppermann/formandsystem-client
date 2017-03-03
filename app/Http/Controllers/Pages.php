6b14403233f4de1667a086021b71c14a4936616f
Lukas Oppermann <lukas@vea.re>
2017-03-03 12:39:22 +0100

    init contentful

--- app/Http/Controllers/Pages.php
+++ app/Http/Controllers/Pages.php
@@ -19,13 +19,20 @@ class Pages extends Controller
 
     public function show(Request $request)
     {
+        $client = new \Contentful\Delivery\Client('d964331125b56d288c3cc7e8517fdd50227decc90f4b8e7c9a539358277a1df2', 'gpw1cuaxmyho');
+        $query = new \Contentful\Delivery\Query;
+        $query->setContentType('page')
+            ->orderBy('fields.order');
+        $entries = $client->getEntries($query);
+        dd($entries);
+
         if(!Cache::has('page-'.$request->path())){
             $view = view('pages.page', [
                 'anchors' => new Anchors()
             ])->render();
             Cache::forever('page-'.$request->path(),$view);
         }
-        
+
         return view('layout.app', [
             'page' => Cache::get('page-'.$request->path())
         ]);
